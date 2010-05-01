6e8a73550cad948d15b92837302bb54d36c85ed8
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-01 14:47:03 +0800

    barebones editor

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -2,4 +2,6 @@ $.couch.app(function(app) {
   $("#hello").evently(app.ddoc.evently.hello, app);
   
   $("#navbar").evently(app.ddoc.evently.navbar, app);
+
+  $("#content").evently(app.ddoc.evently.content, app);
 });
