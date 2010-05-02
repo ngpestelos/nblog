33e2b9e39eb56cf6cf408f203dfd9fac019d26bf
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-02 16:40:24 +0800

    encode markdown

--- evently/content/show/data.js
+++ evently/content/show/data.js
@@ -1,3 +1,8 @@
 function(e, resp) {
-  return resp;
+  var app = $$(this).app;
+  var markdown = app.require("vendor/couchapp/lib/markdown");
+  return {
+    title: resp.title,
+    post: markdown.encode(resp.post)
+  };
 }
