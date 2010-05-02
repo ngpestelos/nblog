2407d8baad39d4388e31acc6b20d1e8dcccf0dac
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-02 16:04:57 +0800

    barebones show

--- evently/content/recent/selectors/ol.recent/_changes/selectors/a/click.js
+++ evently/content/recent/selectors/ol.recent/_changes/selectors/a/click.js
@@ -1,4 +1,12 @@
 function() {
-  alert('click event');
+  var li = $(this).parents("li");
+  var entry_id = li.attr("data-id");
+  var app = $$(this).app;
+  var widget = $(this);
+  app.db.openDoc(entry_id, {
+    success: function(doc) {
+      widget.trigger("show", [doc]);
+    }
+  }); 
   return false;
 }
