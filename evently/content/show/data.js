18c47bd331a6823b745ad67eac0fd950a687c4fd
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-02 17:18:34 +0800

    include id when showing entry

--- evently/content/show/data.js
+++ evently/content/show/data.js
@@ -2,6 +2,7 @@ function(e, resp) {
   var app = $$(this).app;
   var markdown = app.require("vendor/couchapp/lib/markdown");
   return {
+    id: resp._id,
     title: resp.title,
     post: markdown.encode(resp.post)
   };
