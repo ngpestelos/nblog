ce74cc3656c8d1cb2985895d60207bc43dc5bac5
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-02 17:38:23 +0800

    basic editor

--- evently/content/show/selectors/a.edit/click.js
+++ evently/content/show/selectors/a.edit/click.js
@@ -1,4 +1,16 @@
+// see evently/content/recent/selectors/ol.recent/_changes/selectors/a/click.js
+
 function() {
-  $(this).trigger("edit", []);
+  var div = $(this).parents("div");
+  var entry_id = div.attr("data-id");
+  var app = $$(this).app;
+  var widget = $(this);
+
+  app.db.openDoc(entry_id, {
+    success: function(doc) {
+      widget.trigger("edit", [doc]);
+    }
+  });
+
   return false;
 }
