a2302f8d894b15d8de09960017dc2d27a1e9ebc2
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-02 18:02:02 +0800

    save other fields (incl. updated_at)

--- evently/content/edit/selectors/form/submit.js
+++ evently/content/edit/selectors/form/submit.js
@@ -6,6 +6,9 @@ function() {
   app.db.openDoc(entry_id, {
     success: function(doc) {
       doc.title = $("input[name=title]").val();
+      doc.post = $("textarea[name=post]").val();
+      doc.tags = $("input[name=tags]").val().split(", ");
+      doc.updated_at = new Date();
       app.db.saveDoc(doc, {
         success: function() {
           widget.trigger("recent", []);
