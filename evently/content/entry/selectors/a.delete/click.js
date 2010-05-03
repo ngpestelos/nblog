fc8bc31e0c93231713541747353dd10d2e5925a1
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-03 20:04:31 +0800

    delete an entry

--- evently/content/entry/selectors/a.delete/click.js
+++ evently/content/entry/selectors/a.delete/click.js
@@ -1,5 +1,15 @@
-function() {
+function(e) {
   var ans = confirm("Are you sure?");
-  alert(ans);
+  if (ans) {
+    var widget = $(this);
+    var id = $("div.entry").attr("data-id");
+    var db = $$(this).app.db;
+    db.openDoc(id, {
+      success: function(doc) {
+        db.removeDoc(doc);
+        widget.trigger("recent", []);
+      }  
+    });
+  }
   return ans;
 }
