e7c18c296f8be80ec36570464fb31fd0fcde5d19
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-05-03 18:28:55 +0800

    added /show pathbinder

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -2,6 +2,7 @@ $.couch.app(function(app) {
   $("#content").evently(app.ddoc.evently.content, app);
   $("#content").pathbinder("recent", "/");
   $("#content").pathbinder("new", "/new");
+  $("#content").pathbinder("show", "/show/:id");
 
   $("#navbar").evently(app.ddoc.evently.navbar, app);
 
