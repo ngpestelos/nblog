7feef78c106c3db46a9d49512e029d9275ac30f7
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-06-27 11:13:25 +0800

    include tag name when displaying the list of tagged entries

--- evently/content/tagged/data.js
+++ evently/content/tagged/data.js
@@ -1,5 +1,8 @@
 function(e, r) {
   var rows = r.rows.reverse(); // reverse sorting hack
+  var tag = r.rows[0].key[0];  
+  //$.log(tag);
+  
   var titles = rows.map(function(t) {
     return {
       id: t.doc._id,
@@ -7,15 +10,6 @@ function(e, r) {
     }
   });
   
-  return {"titles" : titles};
+  return {"titles" : titles, "tag" : tag};
   
-  /*
-  var titles = r.rows.map(function(t) {
-    return {
-      id: t.doc._id,
-      title: t.doc.title
-    }
-  });
-
-  return {"titles": titles};*/
 }
